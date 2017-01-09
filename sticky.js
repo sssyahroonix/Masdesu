14a9e1dcacde76d484aa133dad9f170a2b73c3cf
sssyahroonix <sssyahroonix@gmail.com>
2017-01-09 18:24:50 +0700

    Update sticky.js

--- sticky.js
+++ sticky.js
@@ -4,12 +4,12 @@
     var stickyTop = $('#HTML4').offset().top;
     var stickyHeight = $('#HTML4').height();
     $(window).scroll(function() {
-      var limit = $('#footer-wrapper').offset().top - stickyHeight - 20; // Jarak berhenti di "#footer-wrapper"
+      var limit = $('#footer-wrapper').offset().top - stickyHeight - 40; // Jarak berhenti di "#footer-wrapper"
       var windowTop = $(window).scrollTop();
       if (stickyTop < windowTop) {
         el.css({
           position: 'fixed',
-          top: 20
+          top: 40
         });
       } else {
         el.css('position', 'static');
